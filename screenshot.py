ede16e1157034ddb7e092dc70006a3bb8fe3f4ec
jhong40 <jhong40@gmail.com>
2023-03-01 11:17:27 -0500

    Create screenshot.py

---
+++ screenshot.py
@@ -0,0 +1,11 @@
+mport asyncio
+from pyppeteer import launch
+
+async def main():
+    browser=await launch(options={'args': ['--no-sandbox']})  # need options part to work
+    page = await browser.newPage()
+    await page.goto('https://example.com')
+    await page.screenshot({'path': 'example.png'})
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
