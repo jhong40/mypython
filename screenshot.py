ba9c37e8b4fd30a333706c39eda188ce517856e9
jhong40 <jhong40@gmail.com>
2023-03-01 11:24:55 -0500

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -5,7 +5,7 @@ async def main():
     browser=await launch(options={'args': ['--no-sandbox']})  # need options part to work
     page = await browser.newPage()
     await page.goto('https://example.com')
-    await page.screenshot({'path': 'example.png'})
+    await page.screenshot({'path': '/var/www/html/a.png'})
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
